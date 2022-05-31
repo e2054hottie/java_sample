b9ae3351cd3b6d1781786cc738f58577e7716655
Kosuke Hotta <kosukehotta@KosukenoMacBook-Pro.local>
2022-05-31 12:55:00 +0900

    新規タスク途中まで

---
+++ src/main/java/controller/task/CreateTask.java
@@ -0,0 +1,19 @@
+package controller.task;
+
+import model.user.Repository;
+import model.user.User;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/user/createtask")
+public class CreateTask extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
